68d6923c47fa22023d4a7ec7f5c13ac51ab743e4
Jelle van Oosterbosch <info@celer-it.nl>
2025-11-26 10:52:00 +0100

    Fixed not able to resolve $command argument of CommandController

--- config/controller.php
+++ config/controller.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Stixx\OpenApiCommandBundle\Controller\CommandController;
+use Stixx\OpenApiCommandBundle\Controller\ArgumentResolver\CommandValueResolver;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $configurator): void {
@@ -15,4 +16,8 @@ return static function (ContainerConfigurator $configurator): void {
     $services
         ->set(CommandController::class)
             ->tag('controller.service_arguments');
+
+    $services
+        ->set(CommandValueResolver::class)
+            ->tag('controller.argument_value_resolver');
 };
