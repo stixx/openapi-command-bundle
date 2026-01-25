f5382ab3095ce524216cda2d4ecbf704450b77bb
Jelle van Oosterbosch <info@celer-it.nl>
2026-01-26 00:32:37 +0100

    Fixed renaming issue in AnnotatedCommand

--- tests/Mock/Routing/src/AnnotatedCommand.php
+++ tests/Mock/Routing/src/AnnotatedCommand.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Stixx\OpenApiCommandBundle\Tests\Mock\Routing\Src;
+namespace Stixx\OpenApiCommandBundle\Tests\Mock\Routing\src;
 
 use OpenApi\Attributes as OA;
 
